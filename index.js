42dca2a33f3405e90dc8dc68e1ef95273bc3d1e1
Coiot <Volpe@Isaacs-iMac.local>
2016-09-19 14:56:05 -0500

    testing

---
+++ index.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var button_name = document.querySelectorAll("button[id]")
+    for (var i = 0; i < button_name.length; i++) {
+        button_name[i].addEventListener('click', function() {
+            color_name = this.id;
+            document.getElementById('color_chosen').innerHTML = color_name;
+        })
+    }
+    document.getElementById("0").addEventListener("click", function() {
+        color_fill = color_name;
+        document.getElementById("0").style.fill = color_fill;
+    })
+    document.getElementById("1").addEventListener("click", function() {
+        color_fill = color_name;
+        document.getElementById("1").style.fill = color_fill;
+    })
+    document.getElementById("2").addEventListener("click", function() {
+        color_fill = color_name;
+        document.getElementById("2").style.fill = color_fill;
+    })
+    document.getElementById("3").addEventListener("click", function() {
+        color_fill = color_name;
+        document.getElementById("3").style.fill = color_fill;
+    })
+    document.getElementById("4").addEventListener("click", function() {
+        color_fill = color_name;
+        document.getElementById("4").style.fill = color_fill;
+    })
+    document.getElementById("5").addEventListener("click", function() {
+        color_fill = color_name;
+        document.getElementById("5").style.fill = color_fill;
+    })
+})
