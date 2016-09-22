aef7a2e453b84000194ca878f9370200859d3179
Coiot <isaac.volpe@gmail.com>
2016-09-21 22:34:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -30,48 +30,4 @@
                 color_fill = color_name;
                 document.getElementById("5").style.fill = color_fill;
             })
-
-            var btn = document.getElementById('Download');
-var svg = document.querySelector('svg');
-var canvas = document.querySelector('canvas');
-
-function triggerDownload (imgURI) {
-  var evt = new MouseEvent('click', {
-    view: window,
-    bubbles: false,
-    cancelable: true
-  });
-
-  var a = document.createElement('a');
-  a.setAttribute('download', 'lotus.png');
-  a.setAttribute('href', imgURI);
-  a.setAttribute('target', '_blank');
-
-  a.dispatchEvent(evt);
-}
-
-btn.addEventListener('click', function () {
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
-  var data = (new XMLSerializer()).serializeToString(svg);
-  var DOMURL = window.URL || window.webkitURL || window;
-
-  var img = new Image();
-  var svgBlob = new Blob([data], {type: 'image/svg+xml;'});
-  var url = DOMURL.createObjectURL(svgBlob);
-
-  img.onload = function () {
-    ctx.drawImage(img, 0, 0);
-    DOMURL.revokeObjectURL(url);
-
-    var imgURI = canvas
-        .toDataURL('image/png')
-        .replace('image/png');
-
-    triggerDownload(imgURI);
-  };
-
-  img.src = url;
-});
-
           })
